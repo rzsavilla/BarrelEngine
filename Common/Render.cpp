97f81b6a308572a3c3094156fba2aa73b5fcb4c6
rzsavilla <rzsavilla@googlemail.com>
2017-01-21 01:31:23 +0000

    Documentation comments

--- Common/Render.cpp
+++ Common/Render.cpp
@@ -1,24 +1,5 @@
 #include <stdafx.h>
-#include "..\Include\Render.h"
-
-void Render::updateCamera(std::shared_ptr<GLSLProgram> shader, QuatCamera* cam)
-{
-	//Pass camera uniforms to shader
-	shader->setUniform("mView", cam->view());				//View matrix
-	shader->setUniform("mProjection", cam->projection());	//Projection matrix
-	shader->setUniform("viewPos", cam->position());			//Camera position
-}
-
-void Render::updateLight(std::shared_ptr<GLSLProgram> shader, Light light)
-{
-	shader->setUniform("lightPosition", light.getPosition());
-	shader->setUniform("lightRadius", light.getRadius());
-
-	//Pass light intesity
-	shader->setUniform("La", light.getAmbient());	//Ambient light
-	shader->setUniform("Ld", light.getDiffuse());	//Diffuse light
-	shader->setUniform("Ls", light.getSpecular());	//Specular light
-}
+#include "Render.h"
 
 Render::Render()
 {
@@ -36,33 +17,20 @@ void Render::init()
 
 void Render::handleMessage(std::shared_ptr<Message> msg)
 {
+	//Place object to be drawn in vector, ready to be drawn
 	if (msg->sID == "Render_Draw") {
 		//Get data from message
 		std::shared_ptr<Drawable> drawable = static_cast<RenderComponent::Draw*>(msg.get())->drawObj;
 		//Store data
 		m_ptrDrawObjects.push_back(drawable);	
 	}
-	else if (msg->sID == "Render_SetCamera") {
-		//Get data from message
-		QuatCamera* cam = static_cast<RenderComponent::SetCamera*>(msg.get())->camera;
-		m_ptrCamera = cam;					//Store pointer for camera object
-	}
-	else if (msg->sID == "Render_SetLights") {
-		//Get data from message
-		std::vector<std::pair<std::string, Light>>* vLights = static_cast<RenderComponent::SetLights*>(msg.get())->vLights;
-		m_ptrLights = vLights;					//Store pointer for vector of lights
-	}
 	else if (msg->sID == "SetWindow") {
 		//Get data from message
 		m_ptrWindow = static_cast<SetWindow*>(msg.get())->window;
 	}
 	else if (msg->sID == "Scene_Reload") {
-		m_ptrDrawObjects.clear();
+		m_ptrDrawObjects.clear();				//Empty array of objects to be drawn
 	}
-	else {
-
-	}
-
 }
 
 void Render::update(float dt)
@@ -72,8 +40,8 @@ void Render::update(float dt)
 	for (auto objIt = m_ptrDrawObjects.begin(); objIt != m_ptrDrawObjects.end(); ++objIt) {
 		(*objIt)->draw();
 	}
-	m_ptrDrawObjects.clear();
+	m_ptrDrawObjects.clear();	//All objects have been drawn, clear vector
 
-	glfwSwapInterval(1);		//VSYNC
+	glfwSwapInterval(1);			//VSYNC
 	glfwSwapBuffers(m_ptrWindow);
 }
--- Include/Render.h
+++ Include/Render.h
@@ -1,3 +1,9 @@
+/**
+* @class	Render
+* @brief	Render component draws drawable objects.
+* Will draw all objects/draw messages passed to it.
+*/
+
 #pragma once
 
 #include <stdafx.h>
@@ -5,57 +11,33 @@
 #include "Drawable.h"
 #include "Light.h"
 
-namespace RenderComponent {
+class Render: public Component {
+private:
+	GLFWwindow* m_ptrWindow;
+	//std::vector<std::pair<std::shared_ptr<GLSLProgram>, std::shared_ptr<Drawable>>> m_ptrDrawObjects;	//Stores pointers for objects that need to be drawn along with shader to be used
+	std::vector<std::shared_ptr<Drawable>> m_ptrDrawObjects;	//Stores pointers for objects that need to be drawn along with shader to be used
+	QuatCamera* m_ptrCamera;											//Pointer to camera in use
+	std::vector<std::pair<std::string, Light>>* m_ptrLights;			//Pointer to vector of lights
+public:
+	Render();	//!< Default constructor
 
+	//! Set pointer for window to draw onto
+	void setWindow(GLFWwindow* ptr);
+
+	void init();										//!< Initialize render component
+	void handleMessage(std::shared_ptr<Message> msg);	//!< Handle messages received by component
+	void update(float dt);								//!< Update render component. Will draw drawable objects
+};
+
+//Messages for communicating with Render Component
+namespace RenderComponent {
+	//Message for render component to draw object
 	struct Draw : public Message {
 		Draw(std::shared_ptr<Drawable> obj)
 			:drawObj(obj)
 		{
 			sID = "Render_Draw";
 		}
-
 		std::shared_ptr<Drawable> drawObj;		//Object to be drawn
-		//std::shared_ptr<GLSLProgram> ptrShader;	//Shader used to draw
-	};
-
-	struct SetCamera : public Message {
-		SetCamera(QuatCamera* cam)
-			:camera(cam)
-		{
-			sID = "Render_SetCamera";
-		}
-		QuatCamera* camera;		//For Camera View and projection matrices
-	};
-
-	struct SetLights : public Message {
-		SetLights(std::vector<std::pair<std::string, Light>>* lights)
-			: vLights(lights)
-		{
-			sID = "Render_SetLights";
-		}
-		std::vector<std::pair<std::string, Light>>* vLights;
 	};
 }
-
-class Render: public Component {
-private:
-	GLFWwindow* m_ptrWindow;
-
-	//std::vector<std::pair<std::shared_ptr<GLSLProgram>, std::shared_ptr<Drawable>>> m_ptrDrawObjects;	//Stores pointers for objects that need to be drawn along with shader to be used
-	std::vector<std::shared_ptr<Drawable>> m_ptrDrawObjects;	//Stores pointers for objects that need to be drawn along with shader to be used
-	QuatCamera* m_ptrCamera;					//Pointer to camera in use
-	std::vector<std::pair<std::string, Light>>* m_ptrLights;			//Pointer to vector of lights
-
-	void updateCamera(std::shared_ptr<GLSLProgram> shader, QuatCamera* cam);
-	void updateLight(std::shared_ptr<GLSLProgram> shader, Light light);
-
-public:
-	Render();
-
-	//Set pointer for window to draw onto
-	void setWindow(GLFWwindow* ptr);
-
-	void init();
-	void handleMessage(std::shared_ptr<Message> msg);
-	void update(float dt);
-};
